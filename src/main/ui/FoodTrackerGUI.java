349e30406ada9efb85ef31eea915d3beb2bdcc02
EPICxFLIPPER <ryanfrase832@icloud.com>
2023-11-01 17:36:19 -0700

    Buttons kind of work, better than they did before this commit

--- src/main/ui/FoodTrackerGUI.java
+++ src/main/ui/FoodTrackerGUI.java
@@ -2,8 +2,11 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FoodTrackerGUI extends JFrame {
+//Represents the GUI for the Food Tracker application
+public class FoodTrackerGUI extends JFrame implements ActionListener {
 
     private static final String TITLE = "Food Tracker";
     private static final int X_DIM = 800;
@@ -12,8 +15,15 @@ public class FoodTrackerGUI extends JFrame {
     private static final Font COOPER_BLACK = new Font("Cooper Black",Font.PLAIN,FONT_SIZE);
 
 
+
     private JMenuBar menuBar;
     private JLabel titleLabel;
+    private JButton recipeMenuButton = new JButton();
+    private JButton pantryMenuButton = new JButton();
+    private JButton canCookButton = new JButton();
+
+
+
 
     public FoodTrackerGUI() {
         super(TITLE);
@@ -21,10 +31,10 @@ public class FoodTrackerGUI extends JFrame {
         init();
         createMenuBar();
         createTitleLabel();
+        createButtons();
 
         pack();
         repaint();
-
 
     }
 
@@ -39,6 +49,7 @@ public class FoodTrackerGUI extends JFrame {
         setTitle(TITLE);
 
         setMinimumSize(new Dimension(X_DIM,Y_DIM));
+        setLayout(null);
     }
 
     //Modifies: This
@@ -77,4 +88,42 @@ public class FoodTrackerGUI extends JFrame {
         this.add(titleLabel);
     }
 
+    //Modifes: This
+    //Efffects: Creates the buttons for the Jframe
+    private void createButtons() {
+        createButton(canCookButton,"Can Cook?",100,200,200,200);
+        createButton(recipeMenuButton,"Recipe Menu",X_DIM * 3 / 4,Y_DIM / 2,200,200);
+        createButton(pantryMenuButton,"Pantry Menu",X_DIM / 4,Y_DIM / 2,200,400);
+
+
+    }
+
+    //Effects: sets the button defaults to the given button
+    private void createButton(JButton button, String label,int x,int y,int width, int height) {
+
+
+
+
+        button.setSize(x,y);
+        button.setText(label);
+        button.addActionListener(this);
+        button.setFocusable(false);
+
+
+        JPanel panel = new JPanel();
+        panel.setBounds(x,y,x,y);
+        panel.setBackground(Color.green);
+
+
+        panel.add(button);
+        panel.setLayout(null);
+
+        this.add(panel);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
